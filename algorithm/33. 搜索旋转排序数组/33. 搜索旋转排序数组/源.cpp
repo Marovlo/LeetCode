3aff535e4c51039ee0c811de2b2503e33988cc7e
Marovlo <359207423@qq.com>
2021-05-11 21:11:03 +0800

    33题

---
+++ algorithm/33. 搜索旋转排序数组/33. 搜索旋转排序数组/源.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+   int search(vector<int>& nums, int target) {
+      int size = nums.size();
+      for (int i = 0; i < size; ++i)
+      {
+         if (target == nums[i])return i;
+      }
+      return -1;
+   }
+};
